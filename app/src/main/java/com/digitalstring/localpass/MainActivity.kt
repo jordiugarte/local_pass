2d5f22d06afb6842291ddd9c29c77ab650b9b175
Jordi Ugarte <jordi933@gmail.com>
2021-07-12 10:45:36 -0400

    Initial commit

---
+++ app/src/main/java/com/digitalstring/localpass/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.digitalstring.localpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
